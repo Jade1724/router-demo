0bed31fd016d5d0bd85735c9e2cde7936ac11dbb
Haruka Ichinose <hic21@uclive.ac.nz>
2022-04-26 01:06:41 +1200

    Setup a router and created three new pages.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupsPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<AllMeetupsPage />} />
+      <Route path="/new-meetup" element={<NewMeetupsPage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
+    </Routes>
+  );
 }
 
 export default App;
