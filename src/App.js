cee9a95903be265d9dfc6128078c6adbbf465575
Haruka Ichinose <hic21@uclive.ac.nz>
2022-04-26 02:18:06 +1200

    Added a MainNavigation component with router links.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupsPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigarion from "./components/layout/MainNavigation";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AllMeetupsPage />} />
-      <Route path="/new-meetup" element={<NewMeetupsPage />} />
-      <Route path="/favorites" element={<FavoritesPage />} />
-    </Routes>
+    <div>
+      <MainNavigarion />
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupsPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
   );
 }
 
