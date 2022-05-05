d05ca39ad42c6e4a7f574785ae88edcbaa987434
Haruka Ichinose <hic21@uclive.ac.nz>
2022-05-06 04:09:56 +1200

    Added context to the app and accessed from MeetupItem.

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -1,36 +1,46 @@
-import { createContext } from 'react';
+import { createContext } from "react";
+import { useState } from "react";
 
 const FavoritesContext = createContext({
-    favorites: [],
-    totalFavorites: 0
+  favorites: [],
+  totalFavorites: 0,
+  addFavorite: (favoriteMeetup) => {},
+  removeFavorite: (meetupId) => {},
+  itemIsFavorite: (meetupId) => {},
 });
 
-function FavoritesContextProvidor(props) {
+export function FavoritesContextProvider(props) {
+  const [userFavorites, setUserFavorites] = useState([]);
 
-    const [userFavorites, setUserFavorites] = useState([]);
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  }
 
-    function addFavoriteHandler(favoriteMeetup) {
-        setUserFavorites((prevUserFavorites) => {
-            return prevUserFavorites.concat(favoriteMeetup);
-        });
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
 
-    }
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some((meetup) => meetup.id === meetupId);
+  }
 
-    function removeFavoriteHandler(meetupId) {
-        setUserFavorites(prevUserFavorites => {
-            return prevUserFavorites.filter(meetup => meetup.id !== meetupId);
-        });
-    }
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+    removeFavorite: removeFavoriteHandler,
+    itemIsFavorite: itemIsFavoriteHandler,
+  };
 
-    function itemIsFavoriteHandler() {
-        return userFavorites.some(meetup => meetup.id !== meetupId);
-    }
-
-    const context = {
-        favorites: userFavorites,
-        totalFavorites: useravorites.length
-    }
-    return <FavoritesContext.Providor value={context}>
-        {props.children}
-    </FavoritesContext.Providor>
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider>
+  );
 }
+
+export default FavoritesContext;
